4ff8e5e36e65d8335b67f4f45573dace3c642342
zeqk.net <zeqk.net@b81e3e4a-4873-11de-b25d-9f9778916a3c>
2009-07-15 14:03:17 +0000

    cada tipo de item está en una clase separada soporte para Box

--- trunk/GeoRSSLibrary/GeoRssItems/IGeoRssItem.cs
+++ trunk/GeoRSSLibrary/GeoRssItems/IGeoRssItem.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Xml;
 
-namespace GeoRSSLibrary
+namespace GeoRSSLibrary.GeoRssItems
 {
     public interface IGeoRssItem
     {
---
+++ trunk/GeoRSSLibrary/GeoRssItems/Polygon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeoRSSLibrary.GeoRssItems
+{
+    public class Polygon : GeoRssItem
+    {
+        #region Fields
+
+        private List<Coordinates> _coordinates;
+	
+        #endregion
+
+        #region Constructors
+
+        public Polygon(string guid, DateTime pubDate, string title, string subTitle,
+            string description, string author, string link, List<Coordinates> coordinates)
+            :base(guid,pubDate,title,subTitle,description,author,link)
+        {
+            this._coordinates = coordinates;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public List<Coordinates> Coordinates
+        {
+            get { return _coordinates; }
+            set { _coordinates = value; }
+        }
+
+        #endregion
+
+    }
+}
